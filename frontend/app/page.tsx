44c5e3558b88e612647238fb7b8cdb33d38e7f77
krisna <krisna.w2010@gmail.com>
2026-02-10 15:55:32 +0800

    added qs and refactor fetch

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,5 @@
-import {Button} from "@/components/ui/button";
 import {getHomePage} from "@/data/loaders";
 import {notFound} from "next/navigation";
-
-async function getStrapiData(path: string) {
-  const baseURL = `http://localhost:1337`
-  try {
-    const response = await fetch(baseURL + path);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 async function loader() {
   const data = await getHomePage();
@@ -21,7 +9,8 @@ async function loader() {
 
 export default async function Home() {
   const strapiData = await loader();
-  const {title, description} = strapiData.data;
+  const {title, description, blocks} = strapiData.data;
+  console.log(blocks);
   return (
     <main className="container mx-auto py-6">
       <h1 className="text-5xl font-bold">{title}</h1>
--- frontend/data/loaders.ts
+++ frontend/data/loaders.ts
@@ -1,9 +1,54 @@
 import {fetchAPI} from "@/utils/fetch-api";
 import {getStrapiURL} from "@/utils/get-strapi-url";
+import qs from "qs";
+
+const homePageQuery = qs.stringify(
+  {
+    populate: {
+      blocks: {
+        on: {
+          "blocks.hero-section": {
+            populate: {
+              image: {
+                fields: [
+                  "url",
+                  "alternativeText"
+                ]
+              },
+              logo: {
+                populate: {
+                  image: {
+                    fields: [
+                      "url",
+                      "alternativeText"
+                    ]
+                  }
+                }
+              },
+              cta: true
+            }
+          },
+          "blocks.info-block": {
+            populate: {
+              image: {
+                fields: [
+                  "url",
+                  "alternativeText"
+                ]
+              },
+              cta: true
+            }
+          }
+        }
+      }
+    }
+  }
+)
 
 export async function getHomePage() {
   const path = "/api/home-page"
   const BASE_URL = getStrapiURL()
   const url = new URL(path, BASE_URL);
+  url.search = homePageQuery;
   return await fetchAPI(url.href, { method: "GET" });
 }
