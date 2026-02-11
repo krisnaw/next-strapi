5f9f68bc643a100ab8b0358692df27b1388b6547
krisna <krisna.w2010@gmail.com>
2026-02-11 16:40:23 +0800

    wip pages

--- frontend/app/[slug]/page.tsx
+++ frontend/app/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import {getPageBySlug} from "@/data/loaders";
 import {notFound} from "next/navigation";
+import {StrapiImage} from "@/components/blocks/StrapiImage";
 
 async function loader(slug: string) {
   const {data} = await getPageBySlug(slug);
@@ -16,7 +17,8 @@ export default async function Page({params}: {
   const { blocks } = await loader(slug);
   return (
     <div>
-      <img src="https://cdn.sanity.io/images/gt37f3ae/production/ccc58de71ce60db5f324318390508abf6deca7f3-1440x281.png?w=3840&q=75&fit=clip&auto=format" alt=""/>
+      <StrapiImage src={blocks.image.url} width={227}
+                   height={120} alt={blocks.image.alternativeText} />
       <h2 className="text-5xl  tracking-tight text-gray-900 sm:text-7xl">
         {blocks.title}
       </h2>
