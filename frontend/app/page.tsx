462d5430792602a4a53713e4e556291722771dac
krisna <krisna.w2010@gmail.com>
2026-02-12 20:32:06 +0800

    fix image err

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -15,7 +15,10 @@ export default async function Home() {
 
   return (
     <main className="container mx-auto py-6 h-screen">
-      <StrapiImage width={500} height={250} src={image.url} alt={image.alternativeText} />
+      {image && (
+        <StrapiImage width={500} height={250} src={image.url} alt={image.alternativeText} />
+      )}
+
       <h1 className="text-5xl font-bold">{title}</h1>
       <p className="text-xl mt-4">{description}</p>
     </main>
